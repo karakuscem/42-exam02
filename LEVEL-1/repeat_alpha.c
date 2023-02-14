dd56fc73adce195065e3892037e558110a515bab
karakuscem <karakuscemenes@gmail.com>
2023-02-14 03:27:26 +0300

    unnecessary lib deleted

--- LEVEL-1/repeat_alpha.c
+++ LEVEL-1/repeat_alpha.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 int get_index(char x)
 {
--- LEVEL-3/add_prime_sum.c
+++ LEVEL-3/add_prime_sum.c
@@ -33,7 +33,6 @@ int is_prime(int num)
     return (1);
 }
 
-#include <stdio.h>
 int main(int argc, char **argv)
 {
     if (argc != 2 || ft_atoi(argv[1]) <= 0)
