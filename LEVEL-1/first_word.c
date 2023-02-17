5f0ba40270b57e2c57b029bfaf151e79cfd93ef6
karakuscem <karakuscemenes@gmail.com>
2023-02-17 09:47:15 +0300

    first_word - sort_list added

---
+++ LEVEL-1/first_word.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        write(1, "\n", 1);
+    else
+    {
+        int i = 0;
+        while(argv[1])
+        {
+            while (argv[1][i])
+            {
+                while (argv[1][i] == '\t' || argv[1][i] == ' ' || argv[1][i] == '\n')
+                    i++;
+                while (argv[1][i] != '\t' && argv[1][i] != ' ' && argv[1][i] != '\n' && argv[1][i])
+                    write(1, &argv[1][i++], 1);
+                break;
+            }
+            break ;
+        }
+        write(1, "\n", 1);
+    }
+}
